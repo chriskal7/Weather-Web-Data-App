c63f8c08cd3720c1e05cc8d78a1dc9fc5ee28506
chris <kalantzis.christos01@gmail.com>
2026-02-16 23:16:04 +0200

    Retrieve and filter real data from open weather map API, plot real data and display real sky condition of up to the next 5 days in the given city, add requirements.txt

---
+++ backend.py
@@ -0,0 +1,20 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    num_values = 8 * forecast_days
+    filtered_data = filtered_data[:num_values]
+
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place='Tokyo', forecast_days=3, kind="Temperature"))
